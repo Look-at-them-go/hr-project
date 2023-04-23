db43ad16ca32db8ebed77eb95cb25d48232e5351
Dragan Djordjevic <GaGzO78@gmail.com>
2023-04-23 22:01:50 +0200

    initial commit

---
+++ project/src/test/java/com/dragan/project/project/HrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragan.project.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
